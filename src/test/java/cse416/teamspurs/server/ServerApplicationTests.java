92a54b9143f081d1efda8382b1215b37b2051237
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-03-14 02:32:26 +0000

    added spring template

---
+++ src/test/java/cse416/teamspurs/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cse416.teamspurs.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
