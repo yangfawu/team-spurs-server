ef41963f70fd8eb15031e447216a3cd5ba724c02
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-04-12 02:33:07 +0000

    Replaced separate geojson models with one and updated all affected layers to reflect changes

--- src/main/java/cse416/teamspurs/server/controller/MapController.java
+++ src/main/java/cse416/teamspurs/server/controller/MapController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cse416.teamspurs.server.constant.Group;
 import cse416.teamspurs.server.constant.State;
-import cse416.teamspurs.server.dto.HeatMapDTO;
-import cse416.teamspurs.server.model.DistrictGeoJson;
-import cse416.teamspurs.server.model.StateGeoJson;
+import cse416.teamspurs.server.model.GeoJson;
 import cse416.teamspurs.server.service.MapService;
 
 @RestController
@@ -24,23 +22,23 @@ public class MapController {
     private MapService service;
 
     @GetMapping("/states")
-    public ResponseEntity<List<StateGeoJson>> getAllStates() {
+    public ResponseEntity<List<GeoJson>> getAllStates() {
         var res = service.getAllStates();
         return ResponseEntity.ok(res);
     }
 
     @GetMapping("/regular/{state}")
-    public ResponseEntity<List<DistrictGeoJson>> getStateMap(
+    public ResponseEntity<List<GeoJson>> getStateMap(
             @PathVariable("state") State state) {
         var res = service.getRegularDistrictMapByState(state);
         return ResponseEntity.ok(res);
     }
 
     @GetMapping("/heat/{state}/{group}")
-    public ResponseEntity<HeatMapDTO> getHeatMap(
+    public ResponseEntity<List<GeoJson>> getHeatMap(
             @PathVariable("state") State state,
             @PathVariable("group") Group group) {
-        var res = service.getHeatDistrictMapByStateAndGroup(state, group);
+        var res = service.getHeatMapByStateAndGroup(state, group);
         return ResponseEntity.ok(res);
     }
 
