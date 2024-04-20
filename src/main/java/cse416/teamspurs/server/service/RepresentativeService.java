edeed55130665e7cee24e4454c94005918af68d3
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-04-20 18:21:39 +0000

    added missing Cacheable imports

--- src/main/java/cse416/teamspurs/server/service/RepresentativeService.java
+++ src/main/java/cse416/teamspurs/server/service/RepresentativeService.java
@@ -3,6 +3,7 @@ package cse416.teamspurs.server.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import cse416.teamspurs.server.constant.State;
