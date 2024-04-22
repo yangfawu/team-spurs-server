3e9d0675a7e26154b7f60d1b43829a8f8eac2c06
David Yeung <dyeung156@gmail.com>
2024-04-22 19:56:52 -0400

    Added heatmap legend and feature model.

---
+++ src/main/java/cse416/teamspurs/server/model/HeatMapFeature.java
@@ -0,0 +1,30 @@
+package cse416.teamspurs.server.model;
+
+import java.util.Map;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import cse416.teamspurs.server.constant.GeoLevel;
+import cse416.teamspurs.server.constant.State;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "heat-map-features")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HeatMapFeature
+{
+    @Id
+    private String id;
+
+    private State state;
+    private HeatLevel level;
+    private Map<Group, Integer> bins;
+    private String title;
+    private Map<Group, Integer> demographic;
+   
+
+}
---
+++ src/main/java/cse416/teamspurs/server/repository/HeatMapFeatureRepository.java
@@ -0,0 +1,12 @@
+package cse416.teamspurs.server.repository;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import cse416.teamspurs.server.constant.State;
+import cse416.teamspurs.server.model.HeatMapFeature;
+
+public interface HeatMapFeatureRepository extends MongoRepository<HeatMapFeature, String> {
+
+    public List<HeatMapFeature> findByStateAndLevel(State state, HeatLevel level);
+}
