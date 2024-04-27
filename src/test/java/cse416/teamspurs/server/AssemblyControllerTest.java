66e92e033ee10aea94666697eeff4070ab6abc08
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-04-27 12:52:31 +0000

    added state-voter-distribution endpoint

--- src/test/java/cse416/teamspurs/server/AssemblyControllerTest.java
+++ src/test/java/cse416/teamspurs/server/AssemblyControllerTest.java
@@ -1,41 +1,39 @@
 package cse416.teamspurs.server;
-import org.junit.jupiter.api.Test;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import cse416.teamspurs.server.service.AssemblyService;
-import cse416.teamspurs.server.model.GeoJsonFeature;
-import cse416.teamspurs.server.constant.State;
-import cse416.teamspurs.server.controller.AssemblyController;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import cse416.teamspurs.server.constant.State;
+import cse416.teamspurs.server.controller.AssemblyController;
+import cse416.teamspurs.server.model.GeoJsonFeature;
+import cse416.teamspurs.server.service.AssemblyService;
 
 @WebMvcTest(AssemblyController.class)
 public class AssemblyControllerTest {
     @MockBean
     AssemblyService service;
 
-    @Test 
-    void doesEachNJGeoJsonFeatureHaveDistrict()
-    {
+    @Test
+    void doesEachNJGeoJsonFeatureHaveDistrict() {
         List<GeoJsonFeature> map = service.getAssemblyPlanByState(State.nj);
 
-        for(GeoJsonFeature feature : map)
-            assertTrue(feature.getProperties() != null && feature.getProperties().containsKey("district") 
-            , "District field does not exist in properties");
+        for (GeoJsonFeature feature : map)
+            assertTrue(feature.getProperties() != null && feature.getProperties().containsKey("district"),
+                    "District field does not exist in properties");
     }
 
-    @Test 
-    void doesEachVAGeoJsonFeatureHaveDistrict()
-    {
+    @Test
+    void doesEachVAGeoJsonFeatureHaveDistrict() {
         List<GeoJsonFeature> map = service.getAssemblyPlanByState(State.va);
 
-        for(GeoJsonFeature feature : map)
-            assertTrue(feature.getProperties() != null && feature.getProperties().containsKey("district") 
-            , "District field does not exist in properties");
+        for (GeoJsonFeature feature : map)
+            assertTrue(feature.getProperties() != null && feature.getProperties().containsKey("district"),
+                    "District field does not exist in properties");
     }
 
 }
